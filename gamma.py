a85f0a3702495c8cd107d92ab26ed160ce93efe5
rzi <rzinkstok@gmail.com>
2016-04-12 08:54:15 +0200

    Sets up simple unittesting

---
+++ gamma.py
@@ -0,0 +1,2 @@
+def gamma(xs, ys, refs, x_value, delta_x, delta_y):
+    return
---
+++ test/test.py
@@ -0,0 +1,11 @@
+from unittest import TestCase
+import numpy
+import gamma
+
+class TestGamma(TestCase):
+    def test1(self):
+        xs = numpy.arange(0,100,1)
+        ref = numpy.ones(100)
+        ys = numpy.zeros(100)
+        self.assertEqual(gamma.gamma(xs, ys, ref, 5, delta_x=1, delta_y=1), 1.0)
+
