44932cdd6815fef90010af3433c7e3f6634e72a7
rzi <rzinkstok@gmail.com>
2016-04-12 10:21:55 +0200

    Implements basic gamma function

--- gamma.py
+++ gamma.py
@@ -1,2 +1,10 @@
+import numpy
+from scipy.interpolate import interp1d
+
 def gamma(xs, ys, refs, x_value, delta_x, delta_y):
-    return
+    dx = numpy.square(xs-x_value)/delta_x**2
+    interpolator = interp1d(xs, refs)
+    ref_y = interpolator(x_value)
+    dy = numpy.square(ys-ref_y)/delta_y**2
+    return numpy.min(numpy.sqrt(dx+dy))
+
